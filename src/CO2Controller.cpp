e937bf437ce34569faeffe1bbb87767b5b129f66
humwerthuz <humberto.rojas.carrasco@gmail.com>
2023-11-22 17:38:56 -0300

    explicit hardware serial instantiation for CO2Controller

--- src/CO2Controller.cpp
+++ src/CO2Controller.cpp
@@ -1,49 +1,51 @@
 #include "CO2Controller.h"
 
-CO2Controller::CO2Controller(int txPin, int rxPin, int co2ValveRelayPin) {
-	this->_mhz19 = new MHZ19();
-	this->_ticksCount = 0;
-	// default serial
-	this->_rxPin = rxPin;
-	this->_txPin = txPin;
-	this->_co2ValveRelayPin = co2ValveRelayPin;
-	pinMode(this->_co2ValveRelayPin, OUTPUT);
+CO2Controller::CO2Controller(int8_t tp, int8_t rp, int uart_nr, int8_t valvePin) {
+    mhz19 = new MHZ19();
+    ticksCount = 0;
+	rxPin = tp;
+    txPin = rp;
+    this->serialPort = new HardwareSerial(uart_nr);
+    co2ValveRelayPin = valvePin;
+	pinMode(this->co2ValveRelayPin, OUTPUT);
 	this->setDeviceName("CO2Controller");
 }
 
 void CO2Controller::init(){
-	Serial1.begin(ZH_BAUDRATE, SERIAL_8N1, this->_rxPin, this->_txPin, false, 20000UL); 
-	this->_mhz19->begin(Serial1);
-	this->_mhz19->autoCalibration(false);
+    if(this->serialPort == nullptr) {
+        log_e("SerialPort is null, CO2Controller cannot continue");
+        return;
+    }
+    this->serialPort->begin(ZH_BAUDRATE, SERIAL_8N1, this->rxPin, this->txPin, false, 20000UL);
+	this->mhz19->begin(*this->serialPort);
+	this->mhz19->autoCalibration(false);
+    this->initialized = true;
 }
 
 void CO2Controller::tick(){
-	if (this->_ticksCount % 6000 == 0) { // exerimentally, 100 ticks is like 12 min, so we'll increase it to 12 hours
-		this->_mhz19->autoCalibration(false);
-		this->logger->logInfo("ABC off command sent. Skipping read this time.");
-		this->_ticksCount ++;
-		return;
-	}
-	
-	int co2reading = this->_mhz19->getCO2();
-	if ( this->logger != NULL ) {
+    if(!this->initialized) {
+        log_e("CO2Controller not in initialized state, cannot continue");
+        return;
+    }
+	int co2reading = this->mhz19->getCO2();
+	if ( this->logger != nullptr ) {
 		char buff[64];
 		snprintf(buff, sizeof(buff), "Device %s got reading %d", this->getDeviceName(), co2reading);
 		this->logger->logInfo(buff);
 	}
-	if ( this->apiSink != NULL ) {
+	if ( this->apiSink != nullptr ) {
 		char buff[5];
 		snprintf(buff, sizeof(buff), "%d", co2reading);
 		this->sinkData("co2_reading", buff, this->getDeviceName());
 	}
 	if ( co2reading >= this->_stopCO2 ) {
-		digitalWrite(this->_co2ValveRelayPin, LOW);
+		digitalWrite(this->co2ValveRelayPin, LOW);
 		this->sinkData("co2_injection", "off", this->getDeviceName());
 	} else if ( co2reading <= this->_startCO2 ) {
-		digitalWrite(this->_co2ValveRelayPin, HIGH);
+		digitalWrite(this->co2ValveRelayPin, HIGH);
 		this->sinkData("co2_injection", "on", this->getDeviceName());
 	}
-	this->_ticksCount ++;
+	this->ticksCount ++;
 }
 
 void CO2Controller::setCO2Range(int startCO2, int stopCO2) {
--- src/CO2Controller.h
+++ src/CO2Controller.h
@@ -1,5 +1,5 @@
-#ifndef	_CO2_CONTROLLER_H
-#define _CO2_CONTROLLER_H
+#ifndef	CO2_CONTROLLER_H
+#define CO2_CONTROLLER_H
 
 #define ZH_BAUDRATE 9600
 
@@ -9,17 +9,19 @@
 
 class CO2Controller : public DeviceController {
 private:
-	uint8_t _txPin;
-	uint8_t _rxPin;
-	uint8_t _co2ValveRelayPin;
-	uint32_t _ticksCount;
-	int _startCO2;
-	int _stopCO2;
-	MHZ19 *_mhz19;
+    bool initialized{false};
+	int8_t txPin{};
+	int8_t rxPin{};
+    HardwareSerial *serialPort{};
+	int8_t co2ValveRelayPin{};
+	uint32_t ticksCount;
+	int _startCO2{};
+	int _stopCO2{};
+	MHZ19 *mhz19;
 public:
-	CO2Controller(int, int, int);
+	CO2Controller(int8_t tp, int8_t rp, int uart_nr, int8_t valvePin);
 	void setCO2Range(int, int);
 	void init();
-	void tick();
+	void tick() override;
 };
 #endif
